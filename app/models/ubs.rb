7e3773d0e1062b7925b825eca0df705b20fe1c45
Washington Silva <w-osilva@hotmail.com>
2018-11-16 23:09:34 -0200

    Adjusting Ubs model to use gem elasticsearch-model

--- app/models/ubs.rb
+++ app/models/ubs.rb
@@ -1,24 +1,43 @@
+require 'elasticsearch/model'
+
 class Ubs < ApplicationRecord
   belongs_to :geocode
   has_one :scores, class_name: "Score", foreign_key: "ubs_id", dependent: :destroy
 
   validates :name, presence: true, uniqueness: {:case_sensitive => false, scope: [:address, :city]}
 
-  # ElasticSearch - https://github.com/ankane/searchkick
+  # ElasticSearch
   # -------------------------------------------------------------
-  searchkick _all: false, callbacks: :async
+  include Elasticsearch::Model
 
-  scope :search_import, -> { includes(:geocode, :scores) }
+  after_commit on: [:create] do
+    IndexerJob.perform_later(self.class.name, :index, self.id)
+  end
+  after_commit on: [:update] do
+    IndexerJob.perform_later(self.class.name, :update, self.id)
+  end
+  after_commit on: [:destroy] do
+    __elasticsearch__.delete_document
+  end
 
-  def search_data
+  def as_indexed_json(options = {})
     {
-      all: [
-        name,
-        address,
-        city,
-        phone,
-        geocode.to_s
-      ].join(",")
+      id: id,
+      name: name,
+      address: address,
+      city: city,
+      phone: phone,
+      geocode:{
+        lat: geocode[:lat].to_f,
+        long: geocode[:long].to_f,
+      },
+      scores:{
+        size: scores[:size].value,
+        adaptation_for_seniors: scores[:adaptation_for_seniors].value,
+        medical_equipment: scores[:medical_equipment].value,
+        medicine: scores[:medicine].value,
+      }
     }
   end
+
 end
