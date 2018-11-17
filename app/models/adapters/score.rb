5e18d3c1678fbe88512cc1164d2436f462087b7f
Washington Silva <w-osilva@hotmail.com>
2018-11-16 23:05:38 -0200

    Enhancement in adapters

--- app/models/adapters/score.rb
+++ app/models/adapters/score.rb
@@ -3,18 +3,18 @@ class Adapters::Score
   def self.from_csv(data)
     data.deep_symbolize_keys!
     {
-      size: parse_score(data[:dsc_estrut_fisic_ambiencia]),
-      adaptation_for_seniors: parse_score(data[:dsc_adap_defic_fisic_idosos]),
-      medical_equipment: parse_score(data[:dsc_equipamentos]),
-      medicine: parse_score(data[:dsc_medicamentos])
+      size: enum_score(data[:dsc_estrut_fisic_ambiencia]),
+      adaptation_for_seniors: enum_score(data[:dsc_adap_defic_fisic_idosos]),
+      medical_equipment: enum_score(data[:dsc_equipamentos]),
+      medicine: enum_score(data[:dsc_medicamentos])
     }
   end
 
-  def self.parse_score(val)
-    case val
-    when "Desempenho mediano ou  um pouco abaixo da média" then 1
-    when "Desempenho acima da média" then 2
-    when "Desempenho muito acima da média" then 3
+  def self.enum_score(val)
+    case
+    when val.match(/muito acima da média/) then 3
+    when val.match(/acima da média/) then 2
+    when val.match(/(mediano|abaixo da média)/) then 1
     else 0
     end
   end
--- app/models/adapters/ubs.rb
+++ app/models/adapters/ubs.rb
@@ -1,19 +1,5 @@
 class Adapters::Ubs
-  # arg data {
-  #   :vlr_latitude=>"-10.9112370014188",
-  #   :vlr_longitude=>"-37.0620775222768",
-  #   :cod_munic=>"280030",
-  #   :cod_cnes=>"3492",
-  #   :nom_estab=>"US OSWALDO DE SOUZA",
-  #   :dsc_endereco=>"TV ADALTO BOTELHO",
-  #   :dsc_bairro=>"GETULIO VARGAS",
-  #   :dsc_cidade=>"Aracaju",
-  #   :dsc_telefone=>"7931791326",
-  #   :dsc_estrut_fisic_ambiencia=>"Desempenho acima da média",
-  #   :dsc_adap_defic_fisic_idosos=>"Desempenho muito acima da média",
-  #   :dsc_equipamentos=>"Desempenho mediano ou  um pouco abaixo da média",
-  #   :dsc_medicamentos=>"Desempenho acima da média"
-  # }
+
   def self.from_csv(data)
     data.deep_symbolize_keys!
     {
